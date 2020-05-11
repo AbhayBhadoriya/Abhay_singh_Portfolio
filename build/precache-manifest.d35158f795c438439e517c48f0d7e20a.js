af5e13e9c9f6625ae43c4dfe871b9e008023e232
AbhayBhadoriya <64248294+AbhayBhadoriya@users.noreply.github.com>
2020-05-11 10:56:51 +0530

    initial commit

---
+++ build/precache-manifest.d35158f795c438439e517c48f0d7e20a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b0161067cf4bfcc930fb48a1471341",
+    "url": "/react-prtfolio/index.html"
+  },
+  {
+    "revision": "c27b82c7dc94dddfbb12",
+    "url": "/react-prtfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fac6c3fe93a3f28a7b8",
+    "url": "/react-prtfolio/static/js/2.21553678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-prtfolio/static/js/2.21553678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27b82c7dc94dddfbb12",
+    "url": "/react-prtfolio/static/js/main.d8a1ce17.chunk.js"
+  },
+  {
+    "revision": "b9e743b3df7f00c0f191",
+    "url": "/react-prtfolio/static/js/runtime-main.14ec2da9.js"
+  }
+]);
